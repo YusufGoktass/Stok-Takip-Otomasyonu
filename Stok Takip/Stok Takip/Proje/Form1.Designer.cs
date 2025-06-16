507f58b42c9d490d796324663edf296f84f98639
Yusuf Göktaş <yusufgoktas181@gmail.com>
2025-06-16 22:21:17 +0300

    Stok Takip

---
+++ Stok Takip/Stok Takip/Proje/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kullanıcıİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünEklemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ürünBilgileriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.satışRaporuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kullanıcıİşlemleriToolStripMenuItem,
+            this.ürünEklemeToolStripMenuItem,
+            this.ürünBilgileriToolStripMenuItem,
+            this.satışRaporuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kullanıcıİşlemleriToolStripMenuItem
+            // 
+            this.kullanıcıİşlemleriToolStripMenuItem.Name = "kullanıcıİşlemleriToolStripMenuItem";
+            this.kullanıcıİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.kullanıcıİşlemleriToolStripMenuItem.Text = "Kullanıcı İşlemleri";
+            this.kullanıcıİşlemleriToolStripMenuItem.Click += new System.EventHandler(this.kullanıcıİşlemleriToolStripMenuItem_Click_1);
+            // 
+            // ürünEklemeToolStripMenuItem
+            // 
+            this.ürünEklemeToolStripMenuItem.Name = "ürünEklemeToolStripMenuItem";
+            this.ürünEklemeToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.ürünEklemeToolStripMenuItem.Text = "Ürün Ekleme";
+            this.ürünEklemeToolStripMenuItem.Click += new System.EventHandler(this.ürünEklemeToolStripMenuItem_Click_1);
+            // 
+            // ürünBilgileriToolStripMenuItem
+            // 
+            this.ürünBilgileriToolStripMenuItem.Name = "ürünBilgileriToolStripMenuItem";
+            this.ürünBilgileriToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.ürünBilgileriToolStripMenuItem.Text = "Ürün Bilgileri";
+            this.ürünBilgileriToolStripMenuItem.Click += new System.EventHandler(this.ürünBilgileriToolStripMenuItem_Click_1);
+            // 
+            // satışRaporuToolStripMenuItem
+            // 
+            this.satışRaporuToolStripMenuItem.Name = "satışRaporuToolStripMenuItem";
+            this.satışRaporuToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.satışRaporuToolStripMenuItem.Text = "Satış Raporu";
+            this.satışRaporuToolStripMenuItem.Click += new System.EventHandler(this.satışRaporuToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(707, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "çıkış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kullanıcıİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ürünEklemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ürünBilgileriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem satışRaporuToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Stok Takip/Stok Takip/Proje/Kullaniciislemleri.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace Proje
+{
+    partial class Kullaniciislemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_telefon = new System.Windows.Forms.TextBox();
+            this.txt_soyadi = new System.Windows.Forms.TextBox();
+            this.txt_Adi = new System.Windows.Forms.TextBox();
+            this.txt_Tc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txt_dogumyılı = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(334, 9);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 69;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(240, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 13);
+            this.label9.TabIndex = 68;
+            this.label9.Text = "İşlem Görecek TC :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(290, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 67;
+            this.label8.Text = "label8";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 66;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 65;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(7, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 64;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = false;
+        
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Müsteri",
+            "Admin"});
+            this.comboBox1.Location = new System.Drawing.Point(102, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 63;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 62;
+            this.label7.Text = "Yetki";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 61;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "Sifre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 59;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Kullanıcı Adı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 57;
+            this.label6.Text = " Soyad";
+            // 
+            // txt_telefon
+            // 
+            this.txt_telefon.Location = new System.Drawing.Point(102, 113);
+            this.txt_telefon.Name = "txt_telefon";
+            this.txt_telefon.Size = new System.Drawing.Size(121, 20);
+            this.txt_telefon.TabIndex = 56;
+            // 
+            // txt_soyadi
+            // 
+            this.txt_soyadi.Location = new System.Drawing.Point(102, 64);
+            this.txt_soyadi.Name = "txt_soyadi";
+            this.txt_soyadi.Size = new System.Drawing.Size(121, 20);
+            this.txt_soyadi.TabIndex = 55;
+            // 
+            // txt_Adi
+            // 
+            this.txt_Adi.Location = new System.Drawing.Point(102, 35);
+            this.txt_Adi.Name = "txt_Adi";
+            this.txt_Adi.Size = new System.Drawing.Size(121, 20);
+            this.txt_Adi.TabIndex = 54;
+            // 
+            // txt_Tc
+            // 
+            this.txt_Tc.Location = new System.Drawing.Point(102, 9);
+            this.txt_Tc.Name = "txt_Tc";
+            this.txt_Tc.Size = new System.Drawing.Size(121, 20);
+            this.txt_Tc.TabIndex = 53;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Telefon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Ad ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "TC";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(7, 90);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(55, 13);
+            this.label14.TabIndex = 74;
+            this.label14.Text = "Duğum yılı";
+            // 
+            // txt_dogumyılı
+            // 
+            this.txt_dogumyılı.Location = new System.Drawing.Point(102, 90);
+            this.txt_dogumyılı.Name = "txt_dogumyılı";
+            this.txt_dogumyılı.Size = new System.Drawing.Size(121, 20);
+            this.txt_dogumyılı.TabIndex = 73;
+            // 
+            // Kullaniciislemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 316);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txt_dogumyılı);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_telefon);
+            this.Controls.Add(this.txt_soyadi);
+            this.Controls.Add(this.txt_Adi);
+            this.Controls.Add(this.txt_Tc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Kullaniciislemleri";
+            this.Text = "Kullaniciislemleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_telefon;
+        private System.Windows.Forms.TextBox txt_soyadi;
+        private System.Windows.Forms.TextBox txt_Adi;
+        private System.Windows.Forms.TextBox txt_Tc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txt_dogumyılı;
+    }
+}
